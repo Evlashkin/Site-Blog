9333fece9ed6e2eb2d15f23d999be09ef91acc88
Evlashkin <evlashkin@gmail.com>
2020-09-28 00:08:01 +0300

    v3

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ class Home(ListView):
     model = Posts
     context_object_name = 'posts'
     template_name = 'blog/index.html'
-    paginate_by = 2
+    paginate_by = 4
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(Home, self).get_context_data(**kwargs)
