148d87659ee668b3c3500a7bc7dda56a3c0ed7aa
Evlashkin <evlashkin@gmail.com>
2020-09-28 23:45:06 +0300

    28.09.2020

--- blog/views.py
+++ blog/views.py
@@ -16,12 +16,21 @@ class Home(ListView):
         return context
 
 
-def index(request):
-    return render(request, template_name='blog/index.html')
+class PostByCategory(ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+    allow_empty = False
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(PostByCategory, self).get_context_data(**kwargs)
+        context['title'] = Category.objects.get(slug=self.kwargs['slug'])
+        return context
 
-def category(request, slug):
-    return render(request, template_name='blog/category.html')
+    def get_queryset(self):
+        cat = Category.objects.get(slug=self.kwargs['slug'])
+        return Posts.objects.filter(category=cat)
+
 
 def get_post(request, slug):
-    return render (request, template_name='blog/category.html')
+    return render(request, template_name='blog/category.html')
